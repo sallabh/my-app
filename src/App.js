79b35d34b23a46633121059cf304b6a243a51a73
sallabh <sallabhk@gmail.com>
2019-11-13 23:46:56 +0530

    Introduced Header component and svg file

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ import ShopPage from './pages/shop/shop.component';
 
 import HomePage from './pages/homepage/homepage.component';
 
+import Header from './components/header/header-component';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage}/>    
       <Route  path='/shop' component={ShopPage}/>
       </Switch>
+      
     </div>
   );
 }
